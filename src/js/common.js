f45bb21ee9b189237ef42081ee2407c972fb12b3
robotrainer <asbu26@gmail.com>
2023-12-11 15:40:55 +0300

    created common functions

---
+++ src/js/common.js
@@ -0,0 +1,35 @@
+export function sortPosts(posts, type) {
+  let sortPosts;
+  switch (type) {
+    case "popular": {
+      sortPosts = posts.toSorted((a, b) => b.view - a.view);
+      break;
+    }
+
+    case "commented": {
+      sortPosts = posts.toSorted((a, b) => b.comments - a.comments);
+      break;
+    }
+
+    default: {
+      sortPosts = posts.toSorted((a, b) => b.publishDate - a.publishDate);
+      break;
+    }
+  }
+
+  return sortPosts;
+}
+
+export function searchPosts(posts, searchStr) {
+  const regSearch = new RegExp(searchStr, "i");
+  const searchedData = searchStr
+    ? posts.filter((elem) => regSearch.test(elem.title))
+    : posts;
+  return searchedData;
+}
+
+export function searchAndSort(posts, state) {
+  const searchedPosts = searchPosts(posts, state.searchStr);
+  const sortedPosts = sortPosts(searchedPosts, state.sortType);
+  return sortedPosts;
+}
