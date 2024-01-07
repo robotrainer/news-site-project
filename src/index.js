13d4d13b07a2c1b81b1fe5a26297f3c0a51e2904
robotrainer <asbu26@gmail.com>
2024-01-07 13:03:26 +0300

    added check auth user

--- src/index.js
+++ src/index.js
@@ -4,17 +4,26 @@ import router from "./js/submodules/spa-router/index.js";
 
 import { Main } from "./js/pages/main/index.js";
 // import { Post } from "./js/pages/post/index.js";
-import {Login} from "./js/pages/auth/login/index.js";
+import { Login } from "./js/pages/auth/login/index.js";
+import { Signup } from "./js/pages/auth/signup/index.js";
 
 const ROOT = document.querySelector("#root");
 
-const routes = [
-  { path: "/", view: Main },
-  { path: "/log-in", view: Login },
-];
+const routes = checkAuth()
+  ? [{ path: "/", view: Main }]
+  : [
+      { path: "/log-in", view: Login },
+      { path: "/sign-up", view: Signup },
+      { path: "/*", view: Login },
+    ];
 
 init();
 
 function init() {
   router.initRouter({ target: ROOT, routes: routes });
+}
+
+function checkAuth() {
+  const auth = sessionStorage.getItem("auth");
+  return Boolean(auth);
 }
